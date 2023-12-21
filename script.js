16c5b79584b8405c3910cb35d346a9f71e392524
democ2s <democ2s@outlook.com>
2023-12-21 14:25:44 +0530

    url issue

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-function chngBgColor() {
-  var ele = document.querySelector(".box");
-  ele.style.backgroundColor = "blue";
-}
-function unDoBgColor() {
-  var ele = document.querySelector(".box");
-  ele.style.backgroundColor = "";
-}
-function createLstItems() {
-  let newItm = document.createElement("li");
-  newItm.innerText = "Added dynamically";
-  document.getElementById("ol").appendChild(newItm);
-}
+//window.location.href
 
-function removeLstItems() {
-  let ol = document.getElementById("ol");
-  let allItems = document.querySelectorAll("#ol li");
-  allItems.forEach((element) => {
-    if (element.className != "static-added") {
-      document.getElementById("ol").removeChild(element);
-    }
-  });
-}
+const baseUrl = window.location.origin + "/JS-Projects/";
+document.getElementById("project1").href = baseUrl + "Project_1/index.html";
