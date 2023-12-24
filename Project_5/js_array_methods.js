0cdadad04bddb3d36a2539ce90e51b5d2b9730ea
democ2s <democ2s@outlook.com>
2023-12-24 17:50:13 +0530

    added more example in js_array_methods.js
    added more example in js_array_methods.js

--- Project_5/js_array_methods.js
+++ Project_5/js_array_methods.js
@@ -1,12 +1,3 @@
-/*
-Explain: 
-let retArray = arr.filter(cb);
-let foundIndex = arr.indexOf(4);
-let foundElement = arr.includes(4);
-let sum = arr.reduce(cb);
-arr.forEach(cb);
-*/
-
 // let retArray = arr.filter();
 let arr = ["indigo", "india", "iglo", "lion", "tiger"];
 let arr2 = arr.filter((e) => {
@@ -21,3 +12,30 @@ arr2 = arr.filter((e) => {
 arr2 = arr.filter((e) => {
   if (e.startsWith("i")) return e;
 });
+
+// IndexOf
+let arr3 = ["indigo", "india", "iglo", "lion", "tiger"];
+let searchWord1 = "india";
+let index = arr3.indexOf(searchWord1);
+console.log(`Index of ${searchWord1} is ${index}`);
+
+// includes
+let arr4 = ["indigo", "india", "iglo", "lion", "tiger"];
+let searchWord2 = "india";
+
+let found = arr4.includes(searchWord2);
+console.log(`element ${searchWord2} found? ${found}`);
+
+// reduce
+let arr5 = [3, 4, 6, 3, 6, 9, 10];
+let ans = arr5.reduce((acc, ele) => {
+  return acc + ele;
+});
+console.log(`Sum of all numbers: ${ans}`);
+
+let arr6 = ["indigo", "india", "iglo", "lion", "tiger"];
+let ans1 = arr6.reduce((acc, ele) => {
+  console.log(`processing ${acc}`);
+  return acc + " [ " + ele + " ] ";
+});
+console.log(`Sum of all words: ${ans1}`);
